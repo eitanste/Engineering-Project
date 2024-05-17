c6c7ce1122c3e7a48cc14aabb3b75fde35e6fb8b
eitanste <83840137+eitanste@users.noreply.github.com>
2024-05-17 14:52:12 +0300

    Update telegram_bot.py
    sends also photo

--- yolov5_deploy/telegram_bot.py
+++ yolov5_deploy/telegram_bot.py
@@ -59,18 +59,34 @@
 
 import requests
 
+TOKEN = '6837047207:AAEgO2dR4sRu-vyqFhdgcTlOb0blIEdtN5M'
+# CHAT_ID = '@ToddlerGuardbot'
 GROUP_CHAT_ID = -1002109878950
+FFMPEG_COMMAND = 'ffmpeg -i rtsp://your_camera_stream -f mpegts -c:v mpeg1video -b:v 800k -r 30 -s 640x480 -bf 0 http://your_streaming_server:port'
+BASE_URL = f"https://api.telegram.org/bot{TOKEN}"
+SEND_MESSAGE = "/sendMessage"
+SEND_PHOTO = "/sendPhoto"
 
 def send_message(message: str) -> int:
     payload = {"chat_id": GROUP_CHAT_ID, "text": message}
-    req = requests.post(NOTIFICATION_URL, data=payload)
+    req = requests.post(BASE_URL + SEND_MESSAGE, data=payload)
     return req.status_code
+
+
+def send_photo(photo_url: str) -> int:
+    with open(photo_url, 'rb') as file:
+        files = {'photo': file}
+        data = {'chat_id': GROUP_CHAT_ID}
+        res = requests.post(BASE_URL + SEND_PHOTO, data=data, files=files)
+        return res.status_code
 
 def alart_push_notification():
     res = send_message("ALART!!!! Danger was detected!")
     print(f"The massage got {res}")
-#     TODO: add photo frame to the telegram
-
+    #     TODO: take photo from the stream and delete the local file
+    photo_path = "child_in_danger_1.jpg"
+    res = send_photo(photo_path)
+    print(f"The massage got {res}")
 
 def main():
     alart_push_notification()
